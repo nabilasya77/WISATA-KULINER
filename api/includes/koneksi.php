6920b45c43c99b6a1265fa5fdae441a9b122ae32
Nabila <emailkamu@gmail.com>
2026-06-30 18:35:55 +0700

    up

--- api/includes/koneksi.php
+++ api/includes/koneksi.php
@@ -2,18 +2,13 @@
 $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com'; 
 $port = 4000; 
 $user = '4Mo4Kxg54E74QBX.root';
-$pass = 'tLneNPcw4uT4LSq9'; // <- SUDAH DIGANTI DENGAN YANG BENAR
+$pass = 'tLneNPcw4uT4LSq9'; 
 $db   = 'wisatakuliner';
 
 $koneksi = mysqli_init();
 
-if (!$koneksi) {
-    die("mysqli_init gagal");
-}
-
-// Pastikan file ca.pem sudah Anda unduh dan letakkan di folder yang sama dengan file ini
-$ca_path = __DIR__ . '/ca.pem';
-mysqli_ssl_set($koneksi, NULL, NULL, $ca_path, NULL, NULL);
+// Set flag agar tidak memverifikasi sertifikat SSL lokal yang bermasalah di Vercel
+mysqli_options($koneksi, MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, false);
 
 $real_connect = @mysqli_real_connect(
     $koneksi, 
