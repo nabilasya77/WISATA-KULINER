1000cca83d2593bc5c60c8e5f99d6bdd6bbf5077
Nabila <emailkamu@gmail.com>
2026-06-30 18:31:46 +0700

    up

--- api/includes/koneksi.php
+++ api/includes/koneksi.php
@@ -2,7 +2,7 @@
 $host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com'; 
 $port = 4000; 
 $user = '4Mo4Kxg54E74QBX.root';
-$pass = 'NSsVg4rAZf4aOsp1K'; 
+$pass = 'tLneNPcw4uT4LSq9'; // <- SUDAH DIGANTI DENGAN YANG BENAR
 $db   = 'wisatakuliner';
 
 $koneksi = mysqli_init();
@@ -11,8 +11,8 @@ if (!$koneksi) {
     die("mysqli_init gagal");
 }
 
-// Menggunakan URL sertifikat publik DigiCert yang umum digunakan TiDB
-$ca_path = 'https://cacerts.digicert.com/DigiCertGlobalRootCA.crt.pem';
+// Pastikan file ca.pem sudah Anda unduh dan letakkan di folder yang sama dengan file ini
+$ca_path = __DIR__ . '/ca.pem';
 mysqli_ssl_set($koneksi, NULL, NULL, $ca_path, NULL, NULL);
 
 $real_connect = @mysqli_real_connect(
@@ -28,9 +28,5 @@ $real_connect = @mysqli_real_connect(
 
 if (!$real_connect) {
     die("Koneksi Database Gagal: " . mysqli_connect_error());
-}
-?>
-if (!$real_connect) {
-    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
